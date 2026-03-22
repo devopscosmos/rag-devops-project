69082f20d4fdaec7c8bb6b4aa593d17b00d12b07
Rama <rama.jadhav.devops@gmail.com>
2026-03-22 20:30:54 +0530

    Changed ref of module output var in dev env while creating tfstate backend

--- terraform/env/dev/backend.tf
+++ terraform/env/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name = var.out_rg_name   
+    resource_group_name = module.rg.out_rg_name   
     storage_account_name = "tfstatesa"
     container_name = "tfstate"
     key = "dev.tfstate"
