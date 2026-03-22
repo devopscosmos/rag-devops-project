048a35a92920394f5bba429c51ce69e3b92f5a53
Rama <rama.jadhav.devops@gmail.com>
2026-03-22 19:56:46 +0530

    disabled sub level respurce provider registration

--- terraform/env/dev/providers.tf
+++ terraform/env/dev/providers.tf
@@ -2,4 +2,5 @@ provider "azurerm" {
   features {
     
   }
+  resource_provider_registrations = "none"  ##to avoid error registering resource provider "Microsoft.Web": unexpected status 409 (409 Conflict) with error: ConflictingConcurrentWriteNotAllowed:
 }
--- terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "rg_name" {
-  description = "The name of the resource group."
-  type        = string
-  
-}
-variable "rg_location" {
-  description = "location"
-  type = string
-}
